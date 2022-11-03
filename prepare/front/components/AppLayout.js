52d2661c68b0ff1a462d4b37301915acb94a3ca9
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-03 20:44:21 +0900

    feat: add grid

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,25 +1,41 @@
 import PropTypes from "prop-types";
 import Link from "next/link";
 import React from "react";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
 
+// app.js와 다르게 AppLayout.js는 일부만 공통인 애들
 const AppLayout = ({ children }) => {
   return (
     <div>
-  
-        <Menu mode = "horizontal">
-          <Menu.Item>
-            <Link href="/">노드버드</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link href="/profile">프로필</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link href="/signup">회원가입</Link>
-          </Menu.Item>
-        </Menu>
- 
-      {children}
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">노드버드</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">프로필</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">회원가입</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Input.Search
+            enterButton
+            style={{ verticalAlign: "middle" }}
+          ></Input.Search>
+        </Menu.Item>
+      </Menu>
+      <Row>
+        {/* xs: 모바일 md: 데스크탑 */}
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          오른쪽 메뉴
+        </Col>
+      </Row>
     </div>
   );
 };
