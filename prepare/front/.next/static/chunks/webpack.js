df4460fcf6a72f1e9efc7c4de61342267e2f5762
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-14 20:35:10 +0900

    feat: add other actions

--- prepare/front/.next/static/chunks/webpack.js
+++ prepare/front/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bdbe3a2645416a4e"; }
+/******/ 		__webpack_require__.h = function() { return "eeca2a41b0c23cd6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -5,8 +5,11 @@ function loginAPI() {
   return axios.post("/api/login");
 }
 
-function* login() {
+function* logIn() {
   try {
+    yield put({
+      type: "LOG_IN_REQUEST",
+    });
     // axios를 통해 받은 결과
     const result = yield call(loginAPI);
     yield put({
@@ -23,18 +26,67 @@ function* login() {
   }
 }
 
+function logOutAPI() {
+  return axios.post("/api/logout");
+}
+
+function* logOut() {
+  try {
+    yield put({
+      type: "ADD_POST_REQUEST",
+    });
+    // axios를 통해 받은 결과
+    const result = yield call(logOutAPI);
+    yield put({
+      type: "ADD_POST_SUCCESS",
+      // 성공 결과는 result.data에 담겨 있음
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "ADD_POST_FAILURE",
+      // 실패 결과는 err.response.data에 담겨 있음
+      data: err.response.data,
+    });
+  }
+}
+
+function addPostAPI() {
+  return axios.post("/api/post");
+}
+
+function* addPost() {
+  try {
+    yield put({
+      type: "ADD_POST_REQUEST",
+    });
+    // axios를 통해 받은 결과
+    const result = yield call(addPostAPI);
+    yield put({
+      type: "ADD_POST_SUCCESS",
+      // 성공 결과는 result.data에 담겨 있음
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "ADD_POST_FAILURE",
+      // 실패 결과는 err.response.data에 담겨 있음
+      data: err.response.data,
+    });
+  }
+}
 function* watchLogin() {
   // 로그인 액션이 들어오면, login 함수를 실행
   // 들어올 때 까지 기다림. 이벤트리스너 같은 역할을 수행
-  yield take("LOG_IN", login);
+  yield take("LOG_IN_REQUEST", logIn);
 }
 
 function* watchLogOut() {
-  yield take("LOG_OUT");
+  yield take("ADD_POST_REQUEST", logOut);
 }
 
 function* watchAddPost() {
-  yield take("ADD_POST");
+  yield take("ADD_POST_REQUEST", addPost);
 }
 
 export default function* rootSaga() {
