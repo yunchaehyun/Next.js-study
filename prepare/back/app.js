1062f9c8f81af8086a64a6ca1b1d13186ea83d3e
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-27 13:14:59 +0900

    feat: add server

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,7 +1,14 @@
 const http = require("http");
 
-http.createServer(() => {
-
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  res.write("Hello node1");
+  res.write("Hello node2");
+  res.write("Hello node3");
+  res.write("Hello node4");
+  res.end("Hello node5");
 });
 
-http.listen(3065);
+server.listen(3065, () => {
+  console.log("서버 실행 중");
+});
