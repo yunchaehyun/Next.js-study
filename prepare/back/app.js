5d5bab6658c2b8adc69a1826f02334a6be1bfe0c
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-27 14:09:15 +0900

    feat: spliting router

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
-
+const postRouter = require("./routes/post");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("hello express");
 });
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.send("hello api");
 });
-app.get("/api/posts", (req, res) => {
+app.get("/posts", (req, res) => {
   res.json([
     {
       id: 1,
@@ -24,15 +24,9 @@ app.get("/api/posts", (req, res) => {
     },
   ]);
 });
-app.post("/api/post", (req, res) => {
-  res.json({
-    id: 1,
-    content: "hello",
-  });
-});
-app.delete("/api/post", (req, res) => {
-  res.json({ id: 1 });
-});
+
+app.use("/post", postRouter);
+
 app.listen(3065, () => {
   console.log("서버 실행 중");
 });
